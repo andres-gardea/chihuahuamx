d65af202ce3df8eec89acb5a4b5d993a56a1eb9d
unfotografomascuu <70489589+unfotografomascuu@users.noreply.github.com>
2024-03-27 01:51:50 -0600

    clima 1.1

--- script.js
+++ script.js
@@ -20,54 +20,54 @@ const initAnimation = () => {
 
 initAnimation();
 
-/*Animaciones del Nav*/
-const tl = gsap.timeline({ paused: true });
+// /*Animaciones del Nav*/
+// const tl = gsap.timeline({ paused: true });
 
-const animateOpenNav = () => {
+// const animateOpenNav = () => {
 
-    tl.to(".nav-container", 0.2, {
-        x: '-33vw',
-        autoAlpha: 1,
-        delay: 0.1,
-    });
+//     tl.to(".nav-container", 0.2, {
+//         x: '-33vw',
+//         autoAlpha: 1,
+//         delay: 0.1,
+//     });
 
-    tl.to("#time-text, .logo-text", 0.2, {
-        color: "#e0e0e0",
-    },
-        "-+0.1");
+//     tl.to("#time-text, .logo-text", 0.2, {
+//         color: "#e0e0e0",
+//     },
+//         "-+0.1");
 
-    tl.from(".social-links > div", 0.4, {
-        opacity: 0,
-        y: 10,
-        stagger: {
-            amount: 0.4,
-        },
-    });
+//     tl.from(".social-links > div", 0.4, {
+//         opacity: 0,
+//         y: 10,
+//         stagger: {
+//             amount: 0.4,
+//         },
+//     });
 
-    tl.to(".contact-link > a", 0.8, {
-        top: 0,
-        ease: "power2.inOut",
-        stagger: {
-            amount: 0.4,
-        },
-    },
-        "-=0.4");
+//     tl.to(".contact-link > a", 0.8, {
+//         top: 0,
+//         ease: "power2.inOut",
+//         stagger: {
+//             amount: 0.4,
+//         },
+//     },
+//         "-=0.4");
 
-    tl.from(".nav-footer", 0.3, {
-        opacity: 0
-    }, "-=0.5").reverse();
-};
+//     tl.from(".nav-footer", 0.3, {
+//         opacity: 0
+//     }, "-=0.5").reverse();
+// };
 
-const openNav = () => {
-    animateOpenNav();
-    const navBtn = document.getElementById("menu-toggle-btn");
-    navBtn.onclick = function (e) {
-        navBtn.classList.toggle("active");
-        tl.reversed(!tl.reversed());
-    };
-};
+// const openNav = () => {
+//     animateOpenNav();
+//     const navBtn = document.getElementById("menu-toggle-btn");
+//     navBtn.onclick = function (e) {
+//         navBtn.classList.toggle("active");
+//         tl.reversed(!tl.reversed());
+//     };
+// };
 
-openNav();
+// openNav();
 
 const photoSection1 = document.querySelector('#scroll-section-1');
 const photoSection2 = document.querySelector('#scroll-section-2');
@@ -79,6 +79,7 @@ const photoSection7 = document.querySelector('#scroll-section-7');
 const photoSection8 = document.querySelector('#scroll-section-8');
 const photoSection9 = document.querySelector('#scroll-section-9');
 const photoSection10 = document.querySelector('#scroll-section-10');
+const photoSection11 = document.querySelector('#scroll-section-11');
 
 function SectionView1() {
 
@@ -92,7 +93,7 @@ function SectionView1() {
     photoSection8.classList.remove("open-section");
     photoSection9.classList.remove("open-section");
     photoSection10.classList.remove("open-section");
-
+    photoSection11.classList.remove("open-section");
 }
 
 function SectionView2() {
@@ -107,7 +108,7 @@ function SectionView2() {
     photoSection8.classList.remove("open-section");
     photoSection9.classList.remove("open-section");
     photoSection10.classList.remove("open-section");
-
+    photoSection11.classList.remove("open-section");
 }
 
 function SectionView3() {
@@ -122,7 +123,7 @@ function SectionView3() {
     photoSection8.classList.remove("open-section");
     photoSection9.classList.remove("open-section");
     photoSection10.classList.remove("open-section");
-
+    photoSection11.classList.remove("open-section");
 }
 
 function SectionView4() {
@@ -137,7 +138,7 @@ function SectionView4() {
     photoSection8.classList.remove("open-section");
     photoSection9.classList.remove("open-section");
     photoSection10.classList.remove("open-section");
-
+    photoSection11.classList.remove("open-section");
 }
 
 function SectionView5() {
@@ -152,7 +153,7 @@ function SectionView5() {
     photoSection8.classList.remove("open-section");
     photoSection9.classList.remove("open-section");
     photoSection10.classList.remove("open-section");
-
+    photoSection11.classList.remove("open-section");
 }
 
 function SectionView6() {
@@ -167,7 +168,7 @@ function SectionView6() {
     photoSection8.classList.remove("open-section");
     photoSection9.classList.remove("open-section");
     photoSection10.classList.remove("open-section");
-
+    photoSection11.classList.remove("open-section");
 }
 
 function SectionView7() {
@@ -182,7 +183,7 @@ function SectionView7() {
     photoSection8.classList.remove("open-section");
     photoSection9.classList.remove("open-section");
     photoSection10.classList.remove("open-section");
-
+    photoSection11.classList.remove("open-section");
 }
 
 function SectionView8() {
@@ -197,7 +198,7 @@ function SectionView8() {
     photoSection8.classList.toggle("open-section");
     photoSection9.classList.remove("open-section");
     photoSection10.classList.remove("open-section");
-
+    photoSection11.classList.remove("open-section");
 }
 
 function SectionView9() {
@@ -212,7 +213,7 @@ function SectionView9() {
     photoSection8.classList.remove("open-section");
     photoSection9.classList.toggle("open-section");
     photoSection10.classList.remove("open-section");
-
+    photoSection11.classList.remove("open-section");
 }
 
 function SectionView10() {
@@ -227,6 +228,23 @@ function SectionView10() {
     photoSection8.classList.remove("open-section");
     photoSection9.classList.remove("open-section");
     photoSection10.classList.toggle("open-section");
+    photoSection11.classList.remove("open-section");
+
+}
+
+function SectionView11() {
+
+    photoSection1.classList.remove("open-section");
+    photoSection2.classList.remove("open-section");
+    photoSection3.classList.remove("open-section");
+    photoSection4.classList.remove("open-section");
+    photoSection5.classList.remove("open-section");
+    photoSection6.classList.remove("open-section");
+    photoSection7.classList.remove("open-section");
+    photoSection8.classList.remove("open-section");
+    photoSection9.classList.remove("open-section");
+    photoSection10.classList.remove("open-section");
+    photoSection11.classList.toggle("open-section");
 }
 
 
@@ -237,3 +255,29 @@ var ChihuahuaTime = function () {
 
 ChihuahuaTime();
 setInterval(ChihuahuaTime, 1000);
+
+
+/*Clima*/
+window.addEventListener('load', () => {
+
+    let temperaturaValor = document.getElementById('temperature-text')
+    let temperaturaDescripcion = document.getElementById('temperature-desc')
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?id=4014338&appid=6639b3c56b4258686b4ae9d53093da35&lang=es`
+
+    fetch(url)
+        .then(response => { return response.json() })
+        .then(data => {
+
+            let temp = Math.round(data.main.temp)
+            temperaturaValor.textContent = `${temp} ° C`
+
+            let desc = data.weather[0].description
+            temperaturaDescripcion.textContent = desc.toUpperCase()
+
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
+})
